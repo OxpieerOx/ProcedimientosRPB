6a9cf05fe782394a9751bd05a0004f876f90eb4b
Pieer Rodas Vereau <pie_er1@hotmail.com>
2024-08-21 23:26:33 -0500

    antes del cambio del reporte de dashboard

--- src/main/java/com/hospitalbelen/procedimientosrp/apiProcedimiento/infraestructura/repository/IProcedimientoRepository.java
+++ src/main/java/com/hospitalbelen/procedimientosrp/apiProcedimiento/infraestructura/repository/IProcedimientoRepository.java
@@ -16,11 +16,23 @@ public interface IProcedimientoRepository extends JpaRepository<Procedimiento, L
 
     List<Procedimiento> findByServicioId(Long idServicio);
 
+    @Query("SELECT " +
+            "COALESCE(c.procedimiento.id, p.id) AS procedimientoId, " +
+            "COALESCE(c.procedimiento.nombre, p.nombre) AS nombreProcedimiento, " +
+            "COUNT(c) AS totalCitas " +
+            "FROM Cita c " +
+            "LEFT JOIN c.programacion prog " +
+            "LEFT JOIN prog.procedimiento p " +
+            "GROUP BY COALESCE(c.procedimiento.id, p.id), COALESCE(c.procedimiento.nombre, p.nombre)")
+    List<Object[]> countCitasByProcedimientoInProgramacion();
+
     @Query("SELECT p.id AS procedimientoId, p.nombre AS nombreProcedimiento, COUNT(c) AS totalCitas " +
             "FROM Cita c " +
             "JOIN c.programacion prog " +
             "JOIN prog.procedimiento p " +
             "GROUP BY p.id, p.nombre")
-    List<Object[]> countCitasByProcedimiento();
+    List<Object[]> countCitasByProcedimientoInCita();
+
+    Procedimiento findByNombre(String nombre);
 
 }
